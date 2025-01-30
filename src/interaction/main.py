b1ab07b5f824884089ea2d94911d03fb577c0ba2
ubiratanfilho <ubfilho16@gmail.com>
2025-01-30 15:06:15 -0300

    Adicionando memória

--- src/interaction/main.py
+++ src/interaction/main.py
@@ -1,62 +1,123 @@
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
-from langgraph.graph import START, StateGraph
 from langchain_core.documents import Document
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.messages import BaseMessage
+from langchain.tools.retriever import create_retriever_tool
+from langchain_core.output_parsers import StrOutputParser
+from langchain import hub
+
+from langgraph.graph import START, StateGraph, END, MessagesState
+from langgraph.prebuilt import ToolNode, tools_condition
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph.message import add_messages
 
 from typing_extensions import List, TypedDict
+
+from typing import Annotated, Sequence
+
 import os
+
 import dotenv
-from langchain import hub
-from langchain_core.prompts import ChatPromptTemplate
 
 dotenv.load_dotenv()
 
 FAISS_PATH = "data/faiss_index"
 
-# Check if the FAISS index exists
+### Loading Vector-Store and Retriever Tool
 if os.path.exists(FAISS_PATH):
     # Load the existing FAISS index
     vector_store = FAISS.load_local(FAISS_PATH, OpenAIEmbeddings(), allow_dangerous_deserialization=True)
 else:
     raise FileNotFoundError(f"FAISS index not found at {FAISS_PATH}")
 
-# Initialize LLM
-llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
-
-# Define prompt for question-answering
-prompt = ChatPromptTemplate.from_messages([
-    ("system", """
-        Você é um assistente que tem acesso a documentos da empresa Hotmart. 
-        Responda apenas questões relacionadas à Hotmart. 
-        Utilize o contexto abaixo para responder à pergunta:
-        {context}
-    """),
-    ("human", "{question}")
-]
+retriever = vector_store.as_retriever()
+retriever_tool = create_retriever_tool(
+    retriever,
+    "retrieve_blog_posts",
+    "Search and returns information about Hotmart.",
 )
+tools = [retriever_tool]
 
-# Define state for application
-class State(TypedDict):
-    question: str
-    context: List[Document]
-    answer: str
+### Defining Nodes
+def agent(state: MessagesState):
+    """
+    Invokes the agent model to generate a response based on the current state. Given
+    the question, it will decide to retrieve using the retriever tool, or simply end.
 
-# Define application steps
-def retrieve(state: State):
-    retrieved_docs = vector_store.similarity_search(state["question"])
-    return {"context": retrieved_docs}
+    Args:
+        state (messages): The current state
 
-def generate(state: State):
-    docs_content = "\n\n".join(doc.page_content for doc in state["context"])
-    messages = prompt.invoke({"question": state["question"], "context": docs_content})
-    response = llm.invoke(messages)
-    return {"answer": response.content}
+    Returns:
+        dict: The updated state with the agent response appended to messages
+    """
+    print("---GENERAL AGENT---")
+    messages = state["messages"]
+    model = ChatOpenAI(temperature=0, model="gpt-4o-mini")
+    model = model.bind_tools(tools)
+    response = model.invoke(messages)
 
-# Compile application and test
-graph_builder = StateGraph(State).add_sequence([retrieve, generate])
-graph_builder.add_edge(START, "retrieve")
-graph = graph_builder.compile()
+    return {"messages": [response]}
+
+def generate(state: MessagesState):
+    """
+    Generate answer
+
+    Args:
+        state (messages): The current state
+
+    Returns:
+        dict: The updated state with re-phrased question
+    """
+    print("---RAG AGENT---")
+    messages = state["messages"]
+    question = messages[0].content
+    last_message = messages[-1]
+    docs = last_message.content
+
+    # Chain
+    prompt = hub.pull("rlm/rag-prompt")
+    llm = ChatOpenAI(model_name="gpt-4o-mini", temperature=0)
+    rag_chain = prompt | llm | StrOutputParser()
+
+    response = rag_chain.invoke({"context": docs, "question": question})
+    
+    return {"messages": [response]}
+
+### Defining the graph
+workflow = StateGraph(MessagesState)
+
+workflow.add_node("agent", agent)
+retrieve = ToolNode([retriever_tool])
+workflow.add_node("retrieve", retrieve)
+workflow.add_node(
+    "generate", generate
+)
+workflow.add_edge(START, "agent")
+
+# Decide whether to retrieve
+workflow.add_conditional_edges(
+    "agent",
+    # Assess agent decision
+    tools_condition,
+    {
+        # Translate the condition outputs to nodes in our graph
+        "tools": "retrieve",
+        END: END,
+    },
+)
+workflow.add_edge("retrieve", "generate")
+workflow.add_edge("generate", END)
+
+graph = workflow.compile(checkpointer=MemorySaver()) # 
+
+# Saving the graph as an image
+with open("images/graph.png", "wb") as f:
+    f.write(graph.get_graph(xray=True).draw_mermaid_png())
 
 if __name__ == "__main__":
-    response = graph.invoke({"question": "O que é a Hotmart?"})
-    print(response["answer"])
+    config = {"configurable": {"thread_id": "1"}}
+    while True:
+        question = input("Usuário: ")
+        response = graph.invoke({"messages": [("user", question)]}, config) # config
+        print("Bot:", response['messages'][-1].content)
